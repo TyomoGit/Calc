ff3e239f92b6c1368d94fb42b58d10a58b4719fd
TyomoMinecraft <riku3dno.1@gmail.com>
2022-08-07 11:19:45 +0900

    Initial Commit

---
+++ Calc/CalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalcApp.swift
+//  Calc
+//
+//  Created by Tyomo on 2022/08/07.
+//
+
+import SwiftUI
+
+@main
+struct CalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
